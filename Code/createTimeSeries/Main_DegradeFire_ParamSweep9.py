906e5b0f8ceecc777a605f29402cfa0b58b51a12
David Infortunio <durs125@yahoo.com>
2020-09-13 16:03:51 -0500

    Update Main_DegradeFire_ParamSweep9.py

--- Code/createTimeSeries/Main_DegradeFire_ParamSweep9.py
+++ Code/createTimeSeries/Main_DegradeFire_ParamSweep9.py
@@ -28,32 +28,34 @@ yr =80
 par_range = np.linspace(150, 600, 2)  # alpha
 param = 'alpha' #THE parameter
 
-#This loops over THE parameter in question to name the files according to which parameter value they have
+#This loops over THE parameter in question to name the files according to which parameter value they have, an then loops over the delay values to run the simulation
 for par in par_range:
     alpha = par #This shoud be the parameter
     path1 = 'PostProcessing/Simulations/{}{}'.format(param,par)
     #path2 = 'Simulations/'+ param + str(par)
     Path(path1).mkdir(parents=True, exist_ok=True)
+    
+#This generates and organizes meta data
     pd.DataFrame([mean_range]).to_csv(path1 + '/0metadata.csv', header=False, index=False)
     pd.DataFrame([cv_range]).to_csv(path1 + '/0metadata.csv', mode='a', header=False, index=False)
     row_of_file_names = []
-    #This loops over the delay mean, mu
     for mu in mean_range:
-        #Below loops over the Coeficient of varience of the delay time
         for cv in cv_range:
             file_name = path1+ '/mean=' + str(mu) + '_CV=' + str(cv)  + '.csv'
             row_of_file_names.append(file_name)
         paths = path1 + '/1metadata.csv'
         pd.DataFrame([row_of_file_names]).to_csv(paths,  mode='a', header=False, index=False)
         row_of_file_names = []
-    dilution = Reaction(np.array([-1], dtype=int), 0, 0, [0, beta, 1, 0], 1, [0])
+   #This ends the metadata creation
+    dilution = Reaction(np.array([-1], dtype=int), 0, 0, [0, beta, 1, 0], 1, [0])#np array used to allow expandability to multi species
     enzymatic_degradation = Reaction(np.array([-1], dtype=int), 0, 0, [0, yr, R0, 1], 1, [0])
     
-    cv =.5
-    for mu in mean_range:
+    cv =.5 # This is a debugging item
+    
+    for mu2 in mean_range:
         #for cv in cv_range:
 
-        pool2.starmap(gillespie_sim, [(mu, cv,alpha,beta,R0 ,C0,yr,param,par,dilution,enzymatic_degradation) for cv in cv_range])
+        pool2.starmap(gillespie_sim, [(mu2, cv2,alpha,beta,R0 ,C0,yr,param,par,dilution,enzymatic_degradation) for cv2 in cv_range])
 
 pool2.close()
 pool2.join()
